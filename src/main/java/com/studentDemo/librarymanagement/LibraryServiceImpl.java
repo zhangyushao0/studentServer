93e7e934b79edc0159696f9f6b0983bd4dbea781
Zhangyushao <2785874028@qq.com>
2023-08-30 03:12:25 +0000

    update libarary

--- src/main/java/com/studentDemo/librarymanagement/LibraryServiceImpl.java
+++ src/main/java/com/studentDemo/librarymanagement/LibraryServiceImpl.java
@@ -1,20 +1,19 @@
 package com.studentDemo.librarymanagement;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Collections;
 import io.grpc.stub.StreamObserver;
 
-
 public class LibraryServiceImpl extends LibraryServiceGrpc.LibraryServiceImplBase {
     @Override
-    public void idJudge(idRequest request, StreamObserver<idResponse> responseObserver) {
+    public void loginto(idRequest request, StreamObserver<idResponse> responseObserver) {
         System.out.println("Request received from client:\n" + request);
-        //1 处理request
-        //2 生成response
-
+        // 1 处理request
+        // 2 生成response
         idResponse response = idResponse.newBuilder()
-                .setId(request.getId())
-                .setName(request.getName())
+                // .setId(request.getId())
+                // .setName(request.getName())
                 .setIdentity("0")
                 .build();
         responseObserver.onNext(response);
